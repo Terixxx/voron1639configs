e8cfb5f85460e7845268f9b01c850d3f09d58f60
Terixxx <asen.notkov@gmail.com>
2023-02-19 10:25:01 +0200

    Autocommit from 2023-02-19 10:25:01
    Klipper on commit: aca0c71
    
    Moonraker on commit: 0a811b9
    
    Mainsail version: v2.4.1

---
+++ script/adjlc920.sh
@@ -0,0 +1,62 @@
+#/bin/bash
+# set the items for the Logitech camera
+echo -n "setting custom camera commands.. "
+/usr/bin/v4l2-ctl -c focus_auto=0
+/usr/bin/v4l2-ctl -c focus_absolute=290
+#/usr/bin/v4l2-ctl -c brightness=-40
+#/usr/bin/v4l2-ctl -c contrast=5
+#/usr/bin/v4l2-ctl -c hue=5
+#/usr/bin/v4l2-ctl -c exposure_absolute=200
+#/usr/bin/v4l2-ctl -c exposure_auto=3
+#/usr/bin/v4l2-ctl -c sharpness=100
+#/usr/bin/v4l2-ctl -c gain=15
+#/usr/bin/v4l2-ctl -c saturation=65
+
+echo "done"
+
+#!/bin/bash
+
+####################################
+#
+# adjlc920 - v4l2-ctl simple setup for logitech c920 for octoprint use. iterate through array
+#            to execute the commands vcmd[]. If there is an error, log the error in
+#            syslog and quit. If all commands execute print success in syslog. I run it
+#            on reboot in rc.local but you can run it from the terminal if you wish
+#
+#            this will work for any camera. the controls will vary based on the cam. use
+#            v4l2-ctl -l to list the controls you can adjust
+#
+# CAUTION:   this will exit your rc.local script if it fails. place it cautiously
+#
+####################################
+
+# quit if v4l2-ctl cannot be found
+# if [ ! -f /usr/bin/v4l2-ctl ]; then 
+  # echo "adjlc920 - /usr/bin/v4l2-ctl unavailable"
+  # exit 2
+  # fi
+
+# # the v4l2-ctl commands I want to run in the order I want to run them 
+# # (to find the controls you can use the command v4l2-ctl -l)
+# vcmd[0]="/usr/bin/v4l2-ctl -c focus_auto=0"
+# #vcmd[1]="/usr/bin/v4l2-ctl -c focus_absolute=440"
+# #vcmd[2]="/usr/bin/v4l2-ctl -c brightness=-60"
+# #vcmd[3]="/usr/bin/v4l2-ctl -c contrast=20"
+# exe=${vcmd[0]%% *}                        # this is the command i.e. /usr/bin/v4l2-ctl
+# logstring="V4L2-CTL"
+
+# for cmds in "${vcmd[@]}"
+# do
+  # cmdarg=${cmds:21}                       # get the argument to command
+  # $cmds                                   # execute v4l2-ctl command(s)
+  # if [[ $? != 0 ]]                        # if error
+  # then
+    # failed=1
+    # logger -t $logstring $cmdarg failed!  # log err to syslog - look up in /var/log/syslog
+    # break                                 # break out of loop
+  # fi
+# done
+
+# if [ -z $failed ]; then logger -t $logstring Success!; fi
+
+# exit
